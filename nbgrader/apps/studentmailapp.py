7a0f7c72ab3b8860d0b6e51e38cf0a7d2eb9a3cc
Jonas Harbig <jharbig@mail.upb.de>
2016-11-14 12:06:53 +0100

    Studentmail: text corrected, add dry-run

--- nbgrader/apps/studentmailapp.py
+++ nbgrader/apps/studentmailapp.py
@@ -22,11 +22,23 @@ class StudentMailApp(NbGrader):
 
         For example
 
-            nbgrader studentmail assignment01´
+            ´nbgrader studentmail assignment01´
 
         to mail all students, who submitted an assignment of assignment01, a mail
         with their results.
-        For example mail to ´studnet@mail.upb.de´: ´Matrikel Nr: 1234567, Punkte in hb2: 1.500000´
+        For example mail to ´studnet@mail.upb.de´:
+
+            Hallo,
+
+            im Anhang befindet sich eine HTML Datei mit Ihrem Ergebnis der Heimübung %s.
+            Sie haben 10.0 Punkte erreicht.
+
+            Bitte anworten Sie nicht auf diese Mail, sie ist automatisch generiert.
+            Bei Rückfragen wenden Sie sich bitte an Ihren Tutor.
+
+        To test it use
+
+            ´nbgrader studentmail assignment01 test´
         """
 
     def _classes_default(self):
@@ -36,11 +48,13 @@ class StudentMailApp(NbGrader):
 
     def start(self):
         super(StudentMailApp, self).start()
-        if len(self.extra_args) != 1:
+        if (len(self.extra_args) == 2 and self.extra_args[1] != "test") or len(self.extra_args) not in {1, 2}:
             self.fail("Assignment id not provided. Usage: nbgrader studentmail assinment_id")
 
         if self.extra_args[0] not in get_assignment_list():
             self.fail("Assingment id does not exist")
+
+        test = len(self.extra_args) == 2
 
         points = get_student_list_point()
         mail = get_student_list_mail()
@@ -52,18 +66,18 @@ class StudentMailApp(NbGrader):
             d["mail"] = mail[p][assignment_name]
             d["points"] = points[p][assignment_name]
             d["matrikelnr"] = p
-            self.send_mail_to(d)
+            self.send_mail_to(d, test)
 
-    def send_mail_to(self, d):
+    def send_mail_to(self, d, test=False):
         d["body"] = """Hallo,
 
-im Anhang befindet sich eine HTML Datei mit Ihrem Ergebniss der Heimübung %s.
+im Anhang befindet sich eine HTML Datei mit Ihrem Ergebnis der Heimübung %s.
 Sie haben %.1f Punkte erreicht.
 
-Bitte anworten Sie nicht auf diese Mail, diese Mail ist automatisch generiert.
-Bei Rückfragen wenden Sie sich bitte an Ihren Tutor oder einen Mitarbeiter.""" % (
+Bitte anworten Sie nicht auf diese Mail, sie ist automatisch generiert.
+Bei Rückfragen wenden Sie sich bitte an Ihren Tutor.""" % (
             d["assignment"], d["points"])
-        d["head"] = "Ergebniss %s, GP1" % (d["assignment"])
+        d["head"] = "Ergebnis %s, GP1" % (d["assignment"])
         d["group"] = get_student_id(d["matrikelnr"], d["assignment"])
         #TODO send mail
         d["html"] = "feedback/%s/%s/%s.html" % (d["group"], d["assignment"], d["assignment"])
@@ -75,6 +89,9 @@ Bei Rückfragen wenden Sie sich bitte an Ihren Tutor oder einen Mitarbeiter.""" 
         if d["mail"] is not "" and d["group"] is not None:
             if not os.path.exists(d["html"]):
                 print("No HTML File in Group", d["group"])
+                return
+            self.log.info("%s MtNr:%s Group:%s Mail: %s Point:%.1f" % ("[not send]" if test else "[send]", d["matrikelnr"], d["group"], d["mail"], d["points"]))
+            if test:
                 return
             d["command"] = 'echo "%s" | mutt -s "%s" -a %s -- %s' %(d["body"], d["head"], d["html"], d["mail"])
             command = subprocess.Popen(d["command"], shell=True)
